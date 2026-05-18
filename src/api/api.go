caa0635c3e31f3019ded439abcca4a1cf3036d02
teboc <bobllor@yahoo.com>
2026-05-18 01:55:02 -0500

    added request ID context to middleware handler

--- src/api/api.go
+++ src/api/api.go
@@ -18,6 +18,7 @@ type ContextKey string
 
 const (
 	CONTEXT_USER_SESSION_KEY ContextKey = "userSession"
+	CONTEXT_REQUEST_ID_KEY   ContextKey = "requestId"
 )
 
 type ApiHandler struct {
@@ -100,6 +101,8 @@ func (ah *ApiHandler) CreateAuthMiddleware(f func(http.ResponseWriter, *http.Req
 //
 // The given function is ran between a logging related tasks. The headers are
 // automatically written within this method.
+//
+// The request ID is written to the context of the http.Request.
 func (ah *ApiHandler) middlewareHandler(f func(http.ResponseWriter, *http.Request)) http.Handler {
 	// expected to be wrapped function from the other method
 	next := http.HandlerFunc(f)
@@ -110,6 +113,8 @@ func (ah *ApiHandler) middlewareHandler(f func(http.ResponseWriter, *http.Reques
 
 		ah.log.Infof("Starting new request | id=%s,method=%s", requestID, r.Method)
 		ah.log.Infof("%s: accessed on agent %s", r.RemoteAddr, r.UserAgent())
+
+		r = r.WithContext(context.WithValue(r.Context(), CONTEXT_REQUEST_ID_KEY, requestID))
 
 		WriteHeaders(w, r)
 
