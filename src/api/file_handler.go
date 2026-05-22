97860ca9bc7303a0f3147d644b0ffbb3cd21c1cc
teboc <bobllor@yahoo.com>
2026-05-22 02:06:07 -0500

    convereted response from File to FileResponse for removal of secrets from response

--- src/api/file_handler.go
+++ src/api/file_handler.go
@@ -2,16 +2,21 @@ package api
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"os"
 
 	dbgateway "github.com/bobllor/cloud-project/src/db_gateway"
+	"github.com/bobllor/cloud-project/src/file"
 	"github.com/bobllor/cloud-project/src/utils"
 	"github.com/bobllor/gologger"
 )
 
 const PARENT_ID_KEY = "parentID"
+const FILE_ID_DOWNLOAD_KEY = "fileId"
 
 var FileGetFileParentRoute = fmt.Sprintf("GET /api/storage/folder/{%s}", PARENT_ID_KEY)
+var FileDownloadFileRoute = fmt.Sprintf("POST /api/download/file/{%s}", FILE_ID_DOWNLOAD_KEY)
 
 const (
 	FileGetFileRootRoute = "GET /api/storage"
@@ -27,6 +32,60 @@ func NewFileHandler(gw *dbgateway.Gateway, logger *gologger.Logger) *FileHandler
 		gateway: gw,
 		deps:    utils.NewDeps(logger),
 	}
+}
+
+// DownloadFile handles downloading a file to the client using a stream on the
+// ResponseWriter.
+//
+// This requires auth due to the use of an account ID.
+func (fh *FileHandler) DownloadFile(w http.ResponseWriter, r *http.Request) {
+	usrInfo, ok := GetRequestContext[*dbgateway.UserSessionInfo](r, CONTEXT_USER_SESSION_KEY)
+	if !ok {
+		fh.deps.Log.Critical("User context is not of type *dbgateway.UserSessionInfo")
+		WriteErrorResponse(w, ErrorUnauthorizedMsg, http.StatusUnauthorized, ReasonUnauthorized)
+		return
+	}
+
+	fileId := r.PathValue(FILE_ID_DOWNLOAD_KEY)
+	if fileId == "" {
+		fh.deps.Log.Warnf("Path value %s is empty", FILE_ID_DOWNLOAD_KEY)
+		WriteErrorResponse(w, ErrorBadDataMsg, http.StatusBadRequest, ReasonBadRequestData)
+		return
+	}
+
+	fh.deps.Log.Debugf("File ID: %s", fileId)
+
+	fi, err := fh.gateway.File.GetFile(usrInfo.AccountId, fileId)
+	if err != nil {
+		fh.deps.Log.Criticalf("Error while attempting to query file: %v", err)
+		WriteErrorResponse(w, ErrorInternalErrorMsg, http.StatusInternalServerError, ReasonInternalError)
+		return
+	}
+	if fi == nil {
+		fh.deps.Log.Warnf("File ID %s does not exist", fileId)
+		WriteErrorResponse(w, ErrorBadDataMsg, http.StatusBadRequest, ReasonBadRequestData)
+		return
+	}
+
+	f, err := os.Open(fi.Path)
+	if err != nil {
+		fh.deps.Log.Criticalf("Failed to open file: %v | Path: %s", err, fi.Path)
+		WriteErrorResponse(w, ErrorInternalErrorMsg, http.StatusInternalServerError, ReasonInternalError)
+		return
+	}
+	defer f.Close()
+
+	n, err := io.Copy(w, f)
+	if err != nil {
+		fh.deps.Log.Criticalf("Failed to write file to stream: %v", err)
+		WriteErrorResponse(w, ErrorInternalErrorMsg, http.StatusInternalServerError, ReasonInternalError)
+		return
+	}
+
+	fh.deps.Log.Infof("Wrote %d bytes to stream", n)
+
+	res := NewApiResponse(true)
+	WriteResponse(w, res)
 }
 
 // GetFiles retrieves a slice of Files based on the account ID and the given
@@ -70,7 +129,8 @@ func (fh *FileHandler) GetFiles(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	res := NewApiResponse(files)
+	convertedFiles := file.ToFileResponses(files...)
+	res := NewApiResponse(convertedFiles)
 	n, err := WriteResponse(w, res)
 	if err != nil {
 		fh.deps.Log.Criticalf("Failed to write response to client: %v", err)
