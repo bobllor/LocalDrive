f2b1abfb1b73c30bb07d761847006e5b522d289a
teboc <bobllor@yahoo.com>
2026-06-06 04:23:30 -0500

    added handler to add folders

--- app.go
+++ app.go
@@ -163,6 +163,7 @@ func createServer(gw *dbgateway.Gateway, logger *gologger.Logger, serverAddress 
 	serv.RegisterHandler(api.FileGetFileRootRoute, ap.CreateAuthMiddleware(ap.FileHandler.GetFiles))
 	serv.RegisterHandler(api.FileGetFileParentRoute, ap.CreateAuthMiddleware(ap.FileHandler.GetFiles))
 	serv.RegisterHandler(api.FilePostDownloadFileRoute, ap.CreateAuthMiddleware(ap.FileHandler.DownloadFile))
+	serv.RegisterHandler(api.FilePostAddFolderRoute, ap.CreateAuthMiddleware(ap.FileHandler.PostAddFolder))
 
 	return serv, nil
 }
