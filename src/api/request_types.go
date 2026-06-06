24a4d407d157e56fe8bacf5123b0038c32e10efa
teboc <bobllor@yahoo.com>
2026-06-06 04:37:19 -0500

    changed FileParentId to a pointer string

--- src/api/request_types.go
+++ src/api/request_types.go
@@ -16,11 +16,11 @@ type RequestUserRegisterInfo struct {
 
 // RequestFileUploadInfo is used to hold metadata of the file being uploaded.
 type RequestFileUploadInfo struct {
-	FileName      string `json:"fileName"`
-	FileSize      int    `json:"fileSize"`
-	FileExtension string `json:"fileExtension"`
-	FileParentId  string `json:"fileParentId"`
-	TotalChunks   int    `json:"totalChunks"`
+	FileName      string  `json:"fileName"`
+	FileSize      int     `json:"fileSize"`
+	FileExtension string  `json:"fileExtension"`
+	FileParentId  *string `json:"fileParentId"`
+	TotalChunks   int     `json:"totalChunks"`
 }
 
 // RequestAddFolderInfo contains the request body for adding a folder to the
